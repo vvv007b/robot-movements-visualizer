5040f2b668db64cf77867a5771206fb55ad92cba
bocharov_n <a.nikitozz@gmail.com>
2016-08-19 12:27:20 +0300

    hotfix

--- src/ru/mcst/RobotGroup/PathsLinking/Vector2D.java
+++ src/ru/mcst/RobotGroup/PathsLinking/Vector2D.java
@@ -3,7 +3,7 @@ package ru.mcst.RobotGroup.PathsLinking;
 /**
  * Created by bocharov_n on 23.06.16.
  */
-public class Vector2D {
+class Vector2D {
     public double x, y;
 
     public Vector2D(double x, double y){
